3cfbb3b45d4b937bd629650451d86a5f4a63cccb
TheChef42 <danielflute@gmail.com>
2022-04-28 21:47:44 +0200

    print table

--- SCA/routes/login.js
+++ SCA/routes/login.js
@@ -7,13 +7,16 @@ var router = express.Router();
 router.post('/',async (req,res)=>{
     try {
         const users = await User.findOne({username:req.body.email})
-        const rating = await Rating.find({username:req.body.email},'rating')
-        console.log(rating)
+        const rating = await Rating.find({username:req.body.email}).select('rating')
+        console.log(JSON.stringify(rating))
+        average = rating.reduce((sum, { rating }) => sum + rating, 0) / rating.length
+        console.log(average)
+        console.log(rating.length)
         if(req.body.email == users.username && req.body.password == users.password){
             req.session.user = req.body.email;
             //res.end('Login successful');
             //res.redirect('/dashboard');
-            res.render('dashboard',{user:users,driver:users.driver,average: {$avg: rating.rating}});
+            res.render('dashboard',{user:users,driver:users.driver,average: average});
         }else{
             res.render('index',{user:users,logout:'invalid password'})
         }
