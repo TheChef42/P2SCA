e1f4956a35943b23abdbd97d5190de94493d8e35
expermentus <christofferbakaa@gmail.com>
2022-05-06 11:34:59 +0200

    login craching

--- SCA/routes/login.js
+++ SCA/routes/login.js
@@ -10,24 +10,26 @@ router.get('/', function(req, res) {
 
 router.post('/',async (req,res)=>{
     const users = await User.findOne({username:req.body.email})
-    if(!users)
-        res.render('login',{logout:'invalid username'})
     const rating = await Rating.find({username:req.body.email}).select('rating')
     average = rating.reduce((sum, { rating }) => sum + rating, 0) / rating.length
-    User.findOne({username: req.body.email}, async function(err, User) {
-        if (err) throw err;
-        // test a matching password
-        User.comparePassword(req.body.password, isMatch = function(err, isMatch) {
+    if(!users){
+        res.render('login',{logout:'invalid username'})
+    }else{
+        User.findOne({username: req.body.email}, async function (err, User) {
             if (err) throw err;
-            if(isMatch){
-                req.session.user = req.body.email;
-                res.redirect('/dashboard')
-                //res.render('dashboard',{user:users,driver:users.driver,average: average});
-            }else{
-                res.render('login',{user:users,logout:'invalid password'})
-            }
+            // test a matching password
+            User.comparePassword(req.body.password, isMatch = function (err, isMatch) {
+                if (err) throw err;
+                if (isMatch) {
+                    req.session.user = req.body.email;
+                    res.redirect('/dashboard')
+                    //res.render('dashboard',{user:users,driver:users.driver,average: average});
+                } else {
+                    res.render('login', {user: users, logout: 'invalid password'})
+                }
+            });
         });
-    });
+    }
 });
 
 module.exports = router;
